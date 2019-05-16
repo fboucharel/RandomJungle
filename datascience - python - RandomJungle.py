610a9033e1d841e81254ad2f0e6bd4eac823bcf1
fboucharel <33088033+fboucharel@users.noreply.github.com>
2019-05-16 20:37:10 +0200

    Update datascience - python - RandomJungle.py

--- datascience - python - RandomJungle.py
+++ datascience - python - RandomJungle.py
@@ -57,51 +57,22 @@ df_y = pd.DataFrame( y , columns = y_var )
 df = pd.concat( [ df_X , df_y ] , axis = 1 )
 
 
-# ## Train / Test split
+# ### Librairies pour modélisation
 
 
 from sklearn.model_selection import train_test_split
 
+from sklearn.ensemble import RandomForestClassifier , AdaBoostClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+ 
 
-
-df_train_all , df_test = train_test_split( df , train_size = 0.9 )
-
-df_train_0 = df_train_all[ df_train_all[ 'y' ] == 0 ]
-df_train_1 = df_train_all[ df_train_all[ 'y' ] == 1 ]
-
-print( 'df_train_all : {0}'.format( df_train_all.shape ) )
-print( 'df_train_0 : {0}'.format( df_train_0.shape ) )
-print( 'df_train_1 : {0}'.format( df_train_1.shape ) )
-
-print( 'df_test : {0}'.format( df_test.shape ) )
-
-
-
-# train dataset :
-
-df_X_train_all = df_train_all[ X_var ]
-df_y_train_all = df_train_all[ y_var ]
-
-X_train_all = df_X_train_all.values
-y_train_all = np.ravel( df_y_train_all.values )
-
-# test dataset :
-
-df_X_test = df_test[ X_var ]
-df_y_test = df_test[ y_var ]
-
-X_test = df_X_test.values
-y_test = np.ravel( df_y_test.values )
-
-
-# ### Librairies pour modélisation
-
-
-
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import accuracy_score
-
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
@@ -110,40 +81,7 @@ from sklearn.metrics import roc_auc_score
 import pickle
 
 
-# ### Modèle Baseline
-
-
-
-# Modèle de référence ( RandomForest ) pour comparaison avec modèle cible ( RandomJungle )
-
-clf = RandomForestClassifier( n_estimators = 10 )
-
-clf.fit( X_train_all , y_train_all )
-
-# Sauvegarde du modèle sur disque
-filename = './mdl/_mdl' + '.sav'
-pickle.dump( clf , open( filename , 'wb' ) )
-    
-    
-# Prédictions sur test dataset
-y_test_pred = clf.predict( X_test )
-    
-# Matrice de confusion sur test dataset
-cfu_mtx = confusion_matrix( y_test , y_test_pred )
-    
-# AUC sur test dataset
-auc = roc_auc_score( y_test , y_test_pred )
-    
-#print( 'confusion matrix (test dataset):\n {0}'.format( cfu_mtx ) )
-print( 'baseline | auc (test dataset) : \n {0:.3f}'.format( auc ) )
-print( 'baseline | confusion matrix (test dataset) : \n {0}'.format( cfu_mtx ) )
-
-
-# ### <font : color = 'orange'> Modèle RandomJungle </font>
-
-# ### K RandomForest = RandomJungle
-
-
+# ### Classificateurs spécialisés :
 
 def RandomJungle( N , K ) :
     
@@ -159,18 +97,50 @@ def RandomJungle( N , K ) :
     lst_randjgl_auc = []
     
     for i in range( N ) :
-      
-        # [ I ] Baseline :
+        
+        # [ 0 ] Train / test split :
+        df_train_all , df_test = train_test_split( df , train_size = 0.9 )
+
+        df_train_0 = df_train_all[ df_train_all[ 'y' ] == 0 ]
+        df_train_1 = df_train_all[ df_train_all[ 'y' ] == 1 ]
+        
+        # [ 0.1 ] train dataset :
+
+        df_X_train_all = df_train_all[ X_var ]
+        df_y_train_all = df_train_all[ y_var ]
+
+        X_train_all = df_X_train_all.values
+        y_train_all = np.ravel( df_y_train_all.values )
+
+        # [ 0.2 ] test dataset :
+
+        df_X_test = df_test[ X_var ]
+        df_y_test = df_test[ y_var ]
+
+        X_test = df_X_test.values
+        y_test = np.ravel( df_y_test.values )
+        
+        # [ 1 ] Baseline :
         
         clf = RandomForestClassifier( n_estimators = 10 )
+        #clf = svm.LinearSVC()
+        #clf = LogisticRegression( random_state=0, solver='lbfgs', multi_class='ovr')
+        #clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2) ) 
+        #clf =  AdaBoostClassifier()
+        #clf = QuadraticDiscriminantAnalysis()
+        #clf = GaussianNB()
+        
         clf.fit( X_train_all , y_train_all )
         y_test_pred = clf.predict( X_test )
         auc = roc_auc_score( y_test , y_test_pred )
         lst_bsl_auc.append( auc )
-        print( '[ i : {0} | baseline | auc : {1:.2f} ]'.format( i , auc ) )
+        print( '[ i : {0} | baseline | auc ( test dataset ) : {1:.4f} ]'.format( i , auc ) )
         
         
-        # [ II ] RandomJungle :
+        # [ 2 ] RandomJungle :
+        
+        # Dictionnaire pour stockage des K prédictions
+        #pred_train = {}
         
         lst_auc = []
         # RandomJungle - Taille des échantillons à mettre en face des fraudes :
@@ -190,6 +160,13 @@ def RandomJungle( N , K ) :
             y_train = np.ravel( df_y_train.values )
     
             clf = RandomForestClassifier( n_estimators = 10 )
+            #clf = svm.LinearSVC()
+            #clf = LogisticRegression( solver='lbfgs', multi_class='ovr' )
+            #clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2) ) 
+            #clf =  AdaBoostClassifier()
+            #clf = QuadraticDiscriminantAnalysis()
+            #clf = GaussianNB()
+            
             clf.fit( X_train , y_train )
      
             # RandomJungle - Sauvegarde des modèles sur disque
@@ -203,8 +180,8 @@ def RandomJungle( N , K ) :
             auc = roc_auc_score( y_test , y_test_pred )
             lst_auc.append( auc )
             #print( 'i : {0} | j : {1} | RandomForest | auc (test dataset) : \n {2}'.format( i , j , auc ) )
+    
         
-        # Dictionnaire pour stockage des prédictions sur test dataset X_test :
         pred = {}
 
         for j in range( K ) :
@@ -217,16 +194,12 @@ def RandomJungle( N , K ) :
         df_X_test_pred = pd.DataFrame( pred )
         df_X_test_pred = df_X_test_pred[ sorted( df_X_test_pred.columns ) ]
         
-        # % modèles contre parmi les K modèles :
         df_X_test_pred[ 'cons' ] = df_X_test_pred.sum( axis = 1 ).apply( lambda x : ( K - x ) / K )
-        # Si moins de 50 % des modèles sont contre alors prédiction = 1, 0 sinon
         df_X_test_pred[ 'y_pred' ] = np.where( df_X_test_pred[ 'cons' ] < 0.50 , 1 , 0 )
-        # y_real pour information :
         df_X_test_pred[ 'y_real' ] = y_test
         
-        # Calcul AUC sur test dataset :
         auc = roc_auc_score( y_test , df_X_test_pred[ 'y_pred' ] )
-        print( '[ i : {0} | RandomJungle | auc : {1:.3f} ]'.format( i , auc ) )
+        print( '[ i : {0} | RandomJungle | auc ( test dataset ) : {1:.4f} ]'.format( i , auc ) )
         lst_randjgl_auc.append( auc )
  
     return lst_bsl_auc , lst_randjgl_auc 
